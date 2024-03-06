522b5dc38b3e4261f630fe9f50ff1dfd7aaca1f5
lufangzhou <lufangzhou@gmail.com>
2024-03-06 21:59:04 +0800

    feat: 学习-课程页面

---
+++ src/components/study/WaterFallList/WaterFallList.vue
@@ -0,0 +1,244 @@
+<script setup>
+  import { onLoad, onReachBottom } from '@dcloudio/uni-app'
+  import { data, list } from './data.js'
+  import { reactive, getCurrentInstance, onMounted } from 'vue'
+  import loadingCom from '@/components/loading.vue'
+  const { proxy } = getCurrentInstance()
+  // type = follow, dynamicState
+  const props = defineProps(['type'])
+
+  const state = reactive({
+    columnNum: 2,
+    minHeightColNum: 1,
+    columnData: {},
+    totalList: [],
+    loading: false,
+    pages: {
+      page: 1,
+      pageSize: 10,
+    },
+  })
+  onMounted(() => {
+    // console.log('mounted')
+    setDefaultData()
+    getData()
+  })
+
+  /*onLoad(() => {
+  setDefaultData()
+  getData()
+})*/
+
+  // 模拟获取数据
+  function getData() {
+    state.loading = true
+    if (state.pages.page === 1) setDefaultData()
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        state.totalList.push(...data)
+        // console.log(95, state.totalList)
+        state.loading = false
+        loadNextItem()
+        resolve()
+      }, 500)
+    })
+  }
+
+  /** @加载更多 **/
+  const loadMore = () => {
+    console.log('加载更多')
+    /*state.pages.page++
+    getData()*/
+  }
+
+  /** @图片加载成功 **/
+  function imageLoad() {
+    getMinHeightColumnNum().then(() => loadNextItem())
+  }
+
+  /** @加载下一个元素 **/
+  function loadNextItem() {
+    const totalLength = Object.values(state.columnData).flat().length
+    if (totalLength == state.totalList.length) return
+    state.columnData[state.minHeightColNum].push(state.totalList[totalLength])
+  }
+
+  /** @获取最小列数 **/
+  async function getMinHeightColumnNum() {
+    let minHeight = Infinity // 初始化为无限大
+    return new Promise((resolve) => {
+      $('.columnItem', proxy).then((columItems) => {
+        columItems.forEach((item, index) => {
+          if (item.height < minHeight) {
+            minHeight = item.height
+            state.minHeightColNum = index + 1
+          }
+        })
+        resolve()
+      })
+    })
+  }
+
+  /** @设置默认数据 **/
+  function setDefaultData() {
+    state.totalList = []
+    Array(state.columnNum)
+      .fill(null)
+      .map((item, i) => {
+        state.columnData[i + 1] = []
+      })
+    console.log(90, state.columnNum)
+  }
+
+  /** @封装[jvideo](https://v.ixigua.com/iNQLjWJS/)查询DOM **/
+  function $(selecter, proxy) {
+    return new Promise((resolve) => {
+      uni
+        .createSelectorQuery()
+        .in(proxy)
+        .selectAll(selecter)
+        .boundingClientRect()
+        .exec((res) => {
+          resolve(res[0])
+        })
+    })
+  }
+  /**********************事件***********************/
+  const goUserInfo = () => {
+    console.log('user')
+  }
+
+  const handleLikeClick = (colIndex, index, isLike) => {
+    state.columnData[colIndex + 1][index].isLike = !isLike
+    let likes = state.columnData[colIndex + 1][index].likes
+    state.columnData[colIndex + 1][index].likes = !isLike ? ++likes : --likes
+  }
+  const goDetail = (item) => {
+    console.log(114, item)
+    console.log(117, props.type)
+    uni.navigateTo({
+      url: `/pages/detail/index?type=${props.type}&row=${JSON.stringify(item)}`,
+    })
+  }
+</script>
+<script>
+  export default {
+    options: { styleIsolation: 'shared' },
+  }
+</script>
+<template>
+  <view class="p-waterfall-list">
+    <scroll-view
+      class="follow-scroll-view"
+      scroll-y="true"
+      scroll-x="false"
+      lower-threshold="150"
+      @scrolltolower="loadMore"
+    >
+      <view class="container">
+        <!--?列数-->
+        <view
+          v-for="(colNum, colIndex) in state.columnNum"
+          :key="colNum"
+          class="columnItem"
+        >
+          <!--?item-->
+          <view
+            v-for="(item, i) in state.columnData[colNum]"
+            :key="i"
+            @click="goDetail(item)"
+          >
+            <div class="item">
+              <img class="pic" :src="item.imgUrl" mode="widthFix" />
+            </div>
+          </view>
+        </view>
+      </view>
+      <loading-com v-show="state.loading"></loading-com>
+    </scroll-view>
+  </view>
+</template>
+<style lang="scss" scoped>
+  .p-waterfall-list {
+    box-sizing: border-box;
+    overflow: hidden;
+    height: 100%;
+    .follow-scroll-view {
+      box-sizing: border-box;
+      // padding: 24rpx;
+      height: 100%;
+    }
+  }
+
+  .container {
+    display: flex;
+    padding: 20rpx;
+    .follow-scroll-view {
+      display: flex;
+      height: 100%;
+    }
+    .columnItem {
+      width: calc(100% / v-bind('state.columnNum'));
+      height: fit-content;
+      & + .columnItem {
+        margin-left: 20rpx;
+      }
+    }
+    .item {
+      // border: solid 1px red;
+      border-radius: 20rpx;
+      margin-bottom: 20rpx;
+      box-sizing: border-box;
+      // padding-bottom: 30rpx;
+      background-color: white;
+      overflow: hidden;
+      .pic {
+        border-radius: 20rpx;
+        width: 100%;
+      }
+      .info {
+        padding: 0 20rpx;
+        text {
+          color: #333333;
+          font-size: 26rpx;
+          @include ellipsis(2);
+          margin: 20rpx 0;
+          margin-top: 0;
+        }
+        .end-row {
+          @include vertical-center;
+          justify-content: space-between;
+          color: #666666;
+          font-size: 22rpx;
+          .left {
+            display: flex;
+            align-items: center;
+            .avatar {
+              margin-right: 10rpx;
+            }
+          }
+          .right {
+            display: flex;
+            align-items: center;
+            image {
+              border-radius: 0;
+              margin-right: 8rpx;
+              width: 34rpx !important;
+              height: 30rpx !important;
+            }
+          }
+          .username,
+          .count {
+            font-size: 22rpx;
+          }
+        }
+      }
+    }
+  }
+  .loading {
+    padding: 20rpx;
+    text-align: center;
+    font-size: 28rpx;
+    color: #aaa;
+  }
+</style>
--- src/pages/study/index.vue
+++ src/pages/study/index.vue
@@ -1,8 +1,43 @@
 <script setup>
   import TabBar from '@/components/TabBar.vue'
   import { getBoundInfo } from '@/utils/index.js'
+  import { ref, reactive } from 'vue'
+  import waterFallList from '@/components/study/WaterFallList/WaterFallList.vue'
+  const { boundTop, boundWidth } = getBoundInfo()
 
   const { statusBarHeight } = getBoundInfo()
+
+  const searchVal = ref('')
+
+  const tabs = reactive({
+    current: 0,
+    list: [
+      { name: '英语', value: 'english', index: 0 },
+      { name: '数学', value: 'math', index: 1 },
+      { name: '音乐', value: 'music', index: 2 },
+      { name: '建筑', value: 'architecture', index: 3 },
+      { name: '管理', value: 'management', index: 4 },
+      { name: '金融', value: 'financial', index: 5 },
+    ],
+  })
+
+  const course = ref([1, 2, 3, 4, 5, 6, 77, 8, 8, 8, 88, 8, , 8, 8, 8, 8])
+
+  const onSearch = () => {}
+
+  const handleTabClick = (item) => {
+    tabs.current = item.index
+  }
+
+  const goCart = () => {
+    console.log('goCart')
+  }
+  const goDetail = (item) => {
+    console.log('goDetail', item)
+  }
+  const loadMore = () => {
+    console.log('load more')
+  }
 </script>
 <script>
   export default {
@@ -11,15 +46,144 @@
 </script>
 <template>
   <view class="p-study" :style="{ paddingTop: statusBarHeight * 2 + 'rpx' }">
+    <!--?搜索row-->
+    <u-navbar bgColor="#A26D37" leftIcon="" :autoBack="false">
+      <template #center>
+        <view class="row-content">
+          <u-search
+            class="input-search"
+            searchIconSize="40rpx"
+            height="58rpx"
+            placeholder="请输入"
+            v-model="searchVal"
+            actionText=""
+            @search="onSearch"
+          ></u-search>
+          <u-icon
+            name="shopping-cart"
+            color="#fff"
+            size="32"
+            @click="goCart"
+          ></u-icon>
+        </view>
+      </template>
+    </u-navbar>
+    <!--?主体-->
+    <view
+      class="content"
+      :style="{
+        marginTop: boundTop + 'px',
+        height: `calc(100vh - ${boundTop * 3 + 140}rpx)`,
+      }"
+    >
+      <!--?tabs-->
+      <view class="tab-row">
+        <u-tabs
+          :current="tabs.current"
+          class="study-tabs-inner"
+          :list="tabs.list"
+          @click="handleTabClick"
+          lineColor="#333"
+          lineWidth="60rpx"
+          lineHeight="2"
+          :scrollable="false"
+          :activeStyle="{
+            color: '#333',
+            fontWeight: 'bold',
+            transform: 'scale(1.05)',
+            fontSize: '32rpx',
+          }"
+          :inactiveStyle="{
+            color: '#666',
+            transform: 'scale(1)',
+            fontSize: '32rpx',
+          }"
+        ></u-tabs>
+      </view>
+      <!--?list-->
+      <scroll-view
+        class="follow-scroll-view"
+        scroll-y="true"
+        scroll-x="false"
+        lower-threshold="150"
+        @scrolltolower="loadMore"
+      >
+        <view class="panel">
+          <view class="item" v-for="(i, index) in course" :key="index">
+            <image
+              src="https://cdn.uviewui.com/uview/swiper/swiper3.png"
+            ></image>
+            <view class="btn" @click="goDetail(i)">课程详情</view>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
     <tab-bar></tab-bar>
   </view>
 </template>
 
 <style scoped lang="scss">
   .p-study {
+    .follow-scroll-view {
+      display: flex;
+      height: calc(100% - 46px);
+    }
+    .content {
+      width: 100%;
+      padding: 20rpx;
+      padding-top: 0;
+      box-sizing: border-box;
+      .tab-row {
+      }
+      .panel {
+        // border: solid 1px green;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20rpx;
+        box-sizing: border-box;
+        padding-top: 20rpx;
+        .item {
+          width: calc(50% - 20rpx);
+          height: 528rpx;
+          background-color: #fff;
+          border-radius: 20rpx;
+          overflow: hidden;
+          position: relative;
+          @include vertical-center;
+          image {
+            width: 100%;
+            height: 100%;
+          }
+          .btn {
+            position: absolute;
+            bottom: 54rpx;
+            background-color: $theme;
+            color: #fff;
+            font-size: 44rpx;
+            width: 260rpx;
+            height: 90rpx;
+            @include vertical-center;
+            border-radius: 45rpx;
+          }
+        }
+      }
+    }
   }
 </style>
 <style lang="scss">
   .p-study {
+    .row-content {
+      box-sizing: border-box;
+      display: flex;
+      align-items: center;
+      position: absolute;
+      left: 20rpx;
+      .input-search {
+        margin-right: -70rpx !important;
+        .u-search__content {
+          width: 400rpx;
+        }
+      }
+    }
   }
 </style>
